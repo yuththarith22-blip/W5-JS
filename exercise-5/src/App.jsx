66ad8556afef9fb02c1069ec446230a43f61b88b
THAL SOTHARITH <yuththarith22@gmail.com>
2026-02-13 09:41:08 +0700

    Done ex5

--- exercise-5/src/App.jsx
+++ exercise-5/src/App.jsx
@@ -1,5 +1,13 @@
-function App() {
-  return <>{/* Your code  here */}</>;
-}
+import Card from './components/Card.jsx'
 
+function App() {
+  return (
+    <>
+        <Card title="Card 1" />
+        <Card title="Card 2" />
+        <Card title="Card 3" />
+        <Card title="Card 4" />
+    </>
+  );
+}
 export default App;
---
+++ exercise-5/src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+
+    return (
+
+        <div className="card">
+            <img className="card-image" src="./assets/1.png" alt="profile picture" />
+            <h2 className="card-title">CiCi</h2>
+            <p className="card-text">IT support</p>
+        </div>
+    );
+
+}
+
+export default Card;
